453427ec92dac0293c74c0e58fb00272472d0f97
Cloud User <centos@ip-172-31-15-220.eu-west-2.compute.internal>
2021-06-10 16:06:19 +0000

    A complete solution to Ex6

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Xuechen Lin
+// Date: 10/6/2021
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -14,4 +14,22 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module RGB_convertor (
+	//add ports
+	input clk,
+	input [2:0] colour,
+	input enable,
+	output [23:0] rgb
+	);
+
+blk_mem_gen_0 RGB (
+  .clka(clk),    // input wire clka
+  .ena(enable),      // input wire ena
+  .wea(1'b0),      // input wire [0 : 0] wea
+  .addra(colour),  // input wire [2 : 0] addra
+  .dina(24'h0),    // input wire [23 : 0] dina
+  .douta(rgb)  // output wire [23 : 0] douta
+);
 
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -1,9 +1,96 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Xuechen Lin
+// Date: 10/6/2021
 //
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+ 	
+//Todo: Regitsers and wires
+	reg clk;
+	reg err;
+	reg enable;
+	reg [2:0] colour;
+	wire [23:0] rgb;
+	
+//Todo: Clock generation
+	initial
+		begin
+		    clk = 0;
+		    forever
+		      #(CLK_PERIOD/2) clk=~clk;//flip the clock signal every half period
+		end			
+
+
+//Todo: User logic
+	initial 
+	begin
+
+	//initialise the variables
+	err = 0;
+	enable = 0;
+	colour = 3'b000;
+
+	#CLK_PERIOD
+	enable = 1;
+	forever begin
+		#CLK_PERIOD
+		
+		//the one bit in the input turns to eight bits in the output with each bit in its coresponding position
+		if((colour[0] != (&rgb[7:0])) || (colour[1] != (&rgb[15:8])) || (colour[2] != (&rgb[23:16])))
+			begin
+			$display("TEST FAILED");
+			err = 1;
+			end
+		
+		//change the input colour
+		colour = colour + 3'b001;
+		end
+	end
+
+	
+
+//Todo: Finish test, check for success
+	initial begin
+	  #500
+	  if (err == 0)
+		$display("***TEST PASSED! :) ***");
+   	  $finish;
+	end 	
+
+//Todo: Instantiate counter module
+	RGB top(
+		.clk (clk),
+		.enable (enable),
+		.colour (colour),
+		.rgb (rgb)
+		);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
