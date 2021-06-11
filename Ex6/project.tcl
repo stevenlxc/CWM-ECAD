bf635a17e41e589a124d865f7cfd6e6bd1c83bbc
Cloud User <centos@ip-172-31-15-220.eu-west-2.compute.internal>
2021-06-11 08:28:20 +0000

    A trial solution to Ex6

--- Ex6/project.tcl
+++ Ex6/project.tcl
@@ -21,6 +21,8 @@ create_fileset -constrset -quiet constraints
 #set_property is_enabled false [get_files ${project_constraints}]
 
 #Todo: add verilog modules here
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name blk_mem_gen_0
+set_property -dict [list CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/CWM-ECAD/Ex6/mem.coe}] [get_ips blk_mem_gen_0]
 read_verilog "top.v"
 read_verilog "top_tb.v"
 
